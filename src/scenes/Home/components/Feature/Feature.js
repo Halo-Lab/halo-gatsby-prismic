f1f509bbd9acb57ad783e3c9a4af429885342ac2
MityaDementiy <d.dementiy@gmail.com>
2021-07-28 19:11:26 +0300

    Style Feature section

--- src/scenes/Home/components/Feature/Feature.js
+++ src/scenes/Home/components/Feature/Feature.js
@@ -3,16 +3,16 @@ import { object } from 'prop-types';
 import { RichText } from 'prismic-reactjs';
 import { GatsbyImage } from 'gatsby-plugin-image';
 
+import { feature, featureDescription } from './Feature.module.scss';
+
 const Feature = ({ primary }) => {
   const { title, description, featureimage: featureImage } = primary;
   const { gatsbyImageData, alt } = featureImage;
 
   return (
-    <div className="feature d-flex justify-content-between mb-5">
-      <div className="flex-fill">
-        <GatsbyImage image={gatsbyImageData} alt={alt} />
-      </div>
-      <div className="feature-description align-self-center text-center">
+    <div className={feature}>
+      <GatsbyImage image={gatsbyImageData} alt={alt} />
+      <div className={featureDescription}>
         <RichText render={title.raw} />
         <RichText render={description.raw} />
       </div>
